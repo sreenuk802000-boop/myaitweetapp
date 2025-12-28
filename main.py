78a1e22580df0aecb27dde3a0853db0f29d857f9
sreenuk802000-boop <sreenuk802000@gmail.com>
2025-12-28 21:44:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ chain=tweet_prompt|gemini_model
 
 
 
-import streamlit as stst.header("Tweet Generator")
+import streamlit as st
 st.header("Tweet Generator")
 st.subheader("Generate tweets using Generative AI")
 topic = st.text_input("Topic")
 num_tweets = st.number_input("Nnumber of tweets", min_value=1, max_value=10, value=1, step=1)
+tweets = None
 if st.button("Generate"):
     tweets=chain.invoke({"number":num_tweets,"topic" :topic})
-st.write(tweets.content)
+if tweets:
+    st.write(tweets.content)
+
 
 
 
