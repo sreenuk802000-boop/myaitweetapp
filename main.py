269d6da7014ffbf2a86d7b71cee58f35ae8876d2
sreenuk802000-boop <sreenuk802000@gmail.com>
2025-12-28 22:10:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 os.environ["GOOGLE_API_KEY"]="AIzaSyAH6A_uQy_Phff-n-5X2nHmWKw_52iJqqk"
 tweet_template="Give me {number} tweets on {topic}"
 tweet_prompt=PromptTemplate(template=tweet_template,input_variables=['number','topic'])
-gemini_model=ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite")
+gemini_model=ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 chain=tweet_prompt|gemini_model
 
 st.header("Tweet Generator")
@@ -16,6 +16,7 @@ num_tweets = st.number_input("Nnumber of tweets", min_value=1, max_value=10, val
 if st.button("Generate"):
     tweets=chain.invoke({"number":num_tweets,"topic" :topic})
 st.write(tweets.content)
+
 
 
 
